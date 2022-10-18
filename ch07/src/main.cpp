e36cc10619da3466eb4702bcffca5f0f43d46a2a
Julia-Run <doracodenow@gmail.com>
2022-10-18 19:55:18 +0800

    2022-1018

--- ch07/src/main.cpp
+++ ch07/src/main.cpp
@@ -5,20 +5,29 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
+#include <unordered_map>
 
 using namespace std;
 
 int main(int argc, char **argv) {
-
+    //
+    cout << "-----------------------use genetic func to add string-------------------" << endl;
     vector<int> u(10, 100);
-    vector<int> v(19,10);
+    vector<int> v(19, 10);
     copy(u.begin(), u.end(), v.begin());
-    for (auto x : v) cout << x << " ";
+    for (auto x: v) cout << x << " ";
 
     vector<string> s{"abc", "def", "=ghi", "--ti"};
     cout << endl;
 //    string t = accumulate(s.begin(), s.end(),string(""));
-    string t = accumulate(s.begin(), s.end(),string()); // cannot just use "" !!!!
-    cout << t << endl;
+    string t = accumulate(s.begin(), s.end(), string()); // cannot just use "" !!!!
+    cout << t << "\n" << endl;
+
+    cout << "-----------------------count string num-------------------" << endl;
+    unordered_map<string, int> count_string;
+    vector<string> bubble{"woman", "woman", "woman", "woman", "can", "can", "code", "code", "code"};
+    for (string &str: bubble) count_string[str]++;
+    for (auto &item: count_string) cout << item.first << ": " << item.second << endl;
+    cout << "\n" << endl;
     return 0;
 }
